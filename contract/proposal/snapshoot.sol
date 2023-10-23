a086af140686997132ab7e7420047eaac7465e36
routye <51522215+routye@users.noreply.github.com>
2023-10-23 16:12:54 +0800

    Update snapshoot.sol

--- contract/proposal/snapshoot.sol
+++ contract/proposal/snapshoot.sol
@@ -1,32 +1,43 @@
-// SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
-interface Iconf{
-    function senator() external view returns(address);
-    function poc() external view returns(address);
-    function stEpoch() external view returns(uint);
-    function voteEpoch() external view returns(uint);
-    function offLine() external view returns(uint);
-    function executEpoch() external view returns(uint);
+interface Iconf {
+    function senator() external view returns (address);
+
+    function poc() external view returns (address);
+
+    function stEpoch() external view returns (uint);
+
+    function voteEpoch() external view returns (uint);
+
+    function senatorNum() external view returns (uint);
+
+    function offLine() external view returns (uint);
+
+    function executEpoch() external view returns (uint);
 }
 
-interface Ipoc{
+interface Ipoc {
     function updateExecuter() external;
 }
 
-interface Isenator{
-    function epochId() external view returns(uint);
-    function executerId() external view returns(uint);
-    function executerIndate() external view returns(uint);
-    function getExecuter() external view returns(address);
-    function isSenator(address) external view returns(bool);
+interface Isenator {
+    function epochId() external view returns (uint);
+
+    function executerId() external view returns (uint);
+
+    function executerIndate() external view returns (uint);
+
+    function getExecuter() external view returns (address);
+
+    function isSenator(address) external view returns (bool);
+
+    function updateExecuter() external;
 }
 
 contract Initialize {
     bool internal initialized;
 
-    modifier init(){
+    modifier init() {
         require(!initialized, "initialized");
         _;
         initialized = true;
@@ -36,11 +47,15 @@ contract Initialize {
 contract snapshoot is Initialize {
     address public conf;
 
-    snapshootProposal[] public snapshoots;
+    snapshootProposal[] internal snapshoots;
 
-    enum Result{PENDING, SUCCESS, FAILED}
+    enum Result {
+        PENDING,
+        SUCCESS,
+        FAILED
+    }
 
-    struct snapshootProposal{
+    struct snapshootProposal {
         uint epochId;
         uint executerId;
         string prHash;
@@ -52,125 +67,217 @@ contract snapshoot is Initialize {
         Result result;
     }
 
-    event SendSnapshootProposal(uint indexed executerId, address executer, string prHash, string prId);
+    event SendSnapshootProposal(
+        uint indexed executerId,
+        address executer,
+        string prHash,
+        string prId
+    );
 
-    modifier onlyExecuter(){
-        require(Isenator(Iconf(conf).senator()).getExecuter() == msg.sender, "access denied: only Executer");
+    modifier onlyExecuter() {
+        require(
+            Isenator(Iconf(conf).senator()).getExecuter() == msg.sender,
+            "access denied: only Executer"
+        );
         _;
     }
 
-    modifier onlySentor(){
-        require(Isenator(Iconf(conf).senator()).isSenator(msg.sender), "access denied: only senator");
+    modifier onlySenator() {
+        require(
+            Isenator(Iconf(conf).senator()).isSenator(msg.sender),
+            "access denied: only senator"
+        );
         _;
     }
 
-    modifier onlyPoc(){
+    modifier onlyPoc() {
         require(Iconf(conf).poc() == msg.sender, "access denied: only poc");
         _;
     }
 
-   function initialize(address _conf) external init{
+    function initialize(address _conf) external init {
         conf = _conf;
     }
 
-    function sendSnapshootProposal(string memory _prHash, string memory _prId) external onlyExecuter{
-        //require(isResolution(), "The latest proposal has no resolution");
-        if (snapshoots.length != 0) require(snapshoots[snapshoots.length -1].proposer != msg.sender, "resubmit");
+    function totalSnapshoots() public view returns (uint256) {
+        return snapshoots.length;
+    }
+
+    function snapshootVoteDetails(
+        uint256 id
+    )
+        public
+        view
+        returns (address[] memory assentors, address[] memory unassentors)
+    {
+        return (snapshoots[id].assentors, snapshoots[id].unAssentors);
+    }
+
+    function sendSnapshootProposal(
+        string memory _prHash,
+        string memory _prId
+    ) external onlyExecuter {
+        if (snapshoots.length != 0)
+            require(
+                snapshoots[snapshoots.length - 1].proposer != msg.sender,
+                "resubmit"
+            );
 
         uint _epochId = Isenator(Iconf(conf).senator()).epochId();
         uint _executerId = Isenator(Iconf(conf).senator()).executerId();
         address[] memory nilArray;
-        snapshoots.push(snapshootProposal(
-            _epochId,
-            _executerId,
-            _prHash,
-            _prId,
-            msg.sender,
-            block.timestamp,
-            nilArray,
-            nilArray,
-            Result.PENDING
-        ));
-        snapshoots[snapshoots.length -1].assentors.push(msg.sender);
+        snapshoots.push(
+            snapshootProposal(
+                _epochId,
+                _executerId,
+                _prHash,
+                _prId,
+                msg.sender,
+                block.timestamp,
+                nilArray,
+                nilArray,
+                Result.PENDING
+            )
+        );
+        snapshoots[snapshoots.length - 1].assentors.push(msg.sender);
 
         emit SendSnapshootProposal(_executerId, msg.sender, _prHash, _prId);
     }
 
-
-    function latestSnapshootProposal() external view returns(uint epochId, uint executerId, string memory prHash, string memory prId, address proposer, uint proposalTime, uint result){
-        snapshootProposal memory sp = snapshoots[snapshoots.length -1];
-        return(sp.epochId, sp.executerId, sp.prHash, sp.prId, sp.proposer, sp.proposalTime, uint(sp.result));
+    function latestSnapshootProposal()
+        external
+        view
+        returns (
+            uint epochId,
+            uint executerId,
+            string memory prHash,
+            string memory prId,
+            address proposer,
+            uint proposalTime,
+            uint result
+        )
+    {
+        snapshootProposal memory sp = snapshoots[snapshoots.length - 1];
+        return (
+            sp.epochId,
+            sp.executerId,
+            sp.prHash,
+            sp.prId,
+            sp.proposer,
+            sp.proposalTime,
+            uint(sp.result)
+        );
     }
 
-    function latestSuccesSnapshootProposal() external view returns(uint epochId, uint executerId, string memory prHash, string memory prId, address proposer, uint proposalTime, uint result){
+    function latestSuccesSnapshootProposal()
+        external
+        view
+        returns (
+            uint epochId,
+            uint executerId,
+            string memory prHash,
+            string memory prId,
+            address proposer,
+            uint proposalTime,
+            uint result
+        )
+    {
         snapshootProposal memory sp;
-        for(uint i = snapshoots.length -1; i >= 0; i--){
-            if (snapshoots[i].result == Result.SUCCESS){
+        for (uint i = snapshoots.length - 1; i >= 0; i--) {
+            if (snapshoots[i].result == Result.SUCCESS) {
                 sp = snapshoots[i];
                 break;
             }
         }
-        return(sp.epochId, sp.executerId, sp.prHash, sp.prId, sp.proposer, sp.proposalTime, uint(sp.result));
+        return (
+            sp.epochId,
+            sp.executerId,
+            sp.prHash,
+            sp.prId,
+            sp.proposer,
+            sp.proposalTime,
+            uint(sp.result)
+        );
     }
 
-
-    function vote(bool v) external onlySentor{
+    function vote(bool v) external onlySenator {
         require(snapshoots.length != 0, "No snapshootProposal");
         require(!isResolution(), "Reached a consensus");
 
-        for (uint i=0; i < snapshoots[snapshoots.length-1].assentors.length; i++){
-                require(snapshoots[snapshoots.length-1].assentors[i] != msg.sender,  "multiple voting");
-            }
-        for (uint i=0; i < snapshoots[snapshoots.length-1].unAssentors.length; i++){
-                require(snapshoots[snapshoots.length-1].unAssentors[i] != msg.sender,  "multiple voting");
-            }
+        for (
+            uint i = 0;
+            i < snapshoots[snapshoots.length - 1].assentors.length;
+            i++
+        ) {
+            require(
+                snapshoots[snapshoots.length - 1].assentors[i] != msg.sender,
+                "multiple voting"
+            );
+        }
+        for (
+            uint i = 0;
+            i < snapshoots[snapshoots.length - 1].unAssentors.length;
+            i++
+        ) {
+            require(
+                snapshoots[snapshoots.length - 1].unAssentors[i] != msg.sender,
+                "multiple voting"
+            );
+        }
 
-        if(v) {
-            snapshoots[snapshoots.length-1].assentors.push(msg.sender);
-            if (snapshoots[snapshoots.length-1].assentors.length >= Iconf(conf).offLine()) snapshoots[snapshoots.length-1].result = Result.SUCCESS;
-        }else{
-            snapshoots[snapshoots.length-1].unAssentors.push(msg.sender);
-            if (snapshoots[snapshoots.length-1].unAssentors.length >= Iconf(conf).offLine()) {
-                snapshoots[snapshoots.length-1].result = Result.FAILED;
-                Ipoc(Iconf(conf).poc()).updateExecuter();
-                }
+        if (v) {
+            snapshoots[snapshoots.length - 1].assentors.push(msg.sender);
+            if (
+                snapshoots[snapshoots.length - 1].assentors.length >=
+                Iconf(conf).offLine()
+            ) snapshoots[snapshoots.length - 1].result = Result.SUCCESS;
+        } else {
+            snapshoots[snapshoots.length - 1].unAssentors.push(msg.sender);
+
+            if (
+                snapshoots[snapshoots.length - 1].unAssentors.length >
+                Iconf(conf).senatorNum() - Iconf(conf).offLine()
+            ) {
+                snapshoots[snapshoots.length - 1].result = Result.FAILED;
+                //Ipoc(Iconf(conf).poc()).updateExecuter();
+                Isenator(Iconf(conf).senator()).updateExecuter();
+            }
         }
     }
 
-
-    function veto() external onlyPoc{
-        if (snapshoots.length != 0) snapshoots[snapshoots.length-1].result = Result.FAILED;
+    function veto() external onlyPoc {
+        if (snapshoots.length != 0)
+            snapshoots[snapshoots.length - 1].result = Result.FAILED;
     }
 
-
-
-    function isResolution() public view returns(bool){
-         if (snapshoots.length == 0 || snapshoots[snapshoots.length-1].result != Result.PENDING) {
-             return true;
-         }else{
-             return false;
-         }
-    }
-
-
-    function  isOutLine() external view returns(bool){
+    function isResolution() public view returns (bool) {
         if (
-                (
-                    (snapshoots.length == 0 || snapshoots[snapshoots.length -1].executerId != Isenator(Iconf(conf).senator()).executerId())
-                    &&
-                    block.timestamp + Iconf(conf).executEpoch() - Iconf(conf).stEpoch() > Isenator(Iconf(conf).senator()).executerIndate()
-                ) ||
-
-                (
-                    snapshoots[snapshoots.length -1].result == Result.PENDING
-                    &&
-                    block.timestamp > snapshoots[snapshoots.length -1].proposalTime + Iconf(conf).voteEpoch()
-                )
-        ){
+            snapshoots.length == 0 ||
+            snapshoots[snapshoots.length - 1].result != Result.PENDING
+        ) {
             return true;
-        }else{
+        } else {
+            return false;
+        }
+    }
+
+    function isOutLine() external view returns (bool) {
+        if (
+            ((snapshoots.length == 0 ||
+                snapshoots[snapshoots.length - 1].executerId !=
+                Isenator(Iconf(conf).senator()).executerId()) &&
+                block.timestamp +
+                    Iconf(conf).executEpoch() -
+                    Iconf(conf).stEpoch() >
+                Isenator(Iconf(conf).senator()).executerIndate()) ||
+            (snapshoots[snapshoots.length - 1].result == Result.PENDING &&
+                block.timestamp >
+                snapshoots[snapshoots.length - 1].proposalTime +
+                    Iconf(conf).voteEpoch())
+        ) {
+            return true;
+        } else {
             return false;
         }
     }
 }
-
